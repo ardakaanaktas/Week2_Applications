10edcb5c2fc5eacf2647e311d2919c02334e3f4f
Arda kaan AKTAŞ <arda_kaan_aktas@yahoo.com>
2024-10-10 09:32:14 +0300

    --

---
+++ VacationApp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace VacationApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Gitmek istediğiniz yer ? (Bodrum/Marmaris/Çeşme) : ");
+			string? place = Console.ReadLine();
+			place = place?.ToLower();
+
+            Console.WriteLine("Kaç kişi olacaksınız ? ");
+			int? personCount = Convert.ToInt32(Console.ReadLine());
+			way:
+            Console.WriteLine("Gidiş için hangi yolu tercih edersiniz ? (Hava yolu için -> \"1\" / Kara yolu için -> \"2\")");
+			int? way = Convert.ToInt32(Console.ReadLine());
+
+			if (way == 1 || way == 2)
+			{
+				switch (place)
+			{
+				case "bodrum":
+					Console.WriteLine("Bodrum hakkında genel bilgi");
+                        if (way == 1 )
+                        {
+                            int price = 4000 * (int)personCount;
+						}
+                        break;
+				case "marmaris":
+					Console.WriteLine("Marmaris genel bilgi");
+					break;
+				case "çeşme":
+					Console.WriteLine("Çeşme genel bilgi");
+					break;
+				default:
+					Console.WriteLine("Maalesef girdiğiniz şehir için planlama yapılamamaktadır.");
+					break;
+			}
+			}
+			else
+			{
+				Console.WriteLine("Hatalı giriş yaptınız.");
+				goto way;
+			}
+			
+		}
+
+
+	}
+}
